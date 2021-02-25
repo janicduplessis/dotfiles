1fea5f144634bf334c64ddbe668b15d865ef7305
Janic Duplessis <janicduplessis@gmail.com>
2021-02-25 01:20:59 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-echo "//registry.npmjs.org/:_authToken=$NPM_ACCESS_TOKEN" > ~/.npmrc
+printenv
+
+if [ -n "$NPM_ACCESS_TOKEN" ]; then
+  echo "//registry.npmjs.org/:_authToken=$NPM_ACCESS_TOKEN" > ~/.npmrc
+fi
